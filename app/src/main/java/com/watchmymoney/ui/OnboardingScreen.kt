689ea9fad2f7afd51db178d461ea0d62d0f544fa
WatchMyMoney Dev <dev@watchmymoney.com>
2026-06-19 17:05:21 +0800

    Remove unused CircleShape import from OnboardingScreen

--- app/src/main/java/com/watchmymoney/ui/OnboardingScreen.kt
+++ app/src/main/java/com/watchmymoney/ui/OnboardingScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Backspace
 import androidx.compose.material.icons.rounded.Check
